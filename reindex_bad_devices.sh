857a33fe1854aa1382c1faf6aa5150efc4a5ceda
Jhonatan M <57998098+zuka1337@users.noreply.github.com>
2020-06-29 12:19:58 +0100

    Update reindex_bad_devices.sh

--- reindex_bad_devices.sh
+++ reindex_bad_devices.sh
@@ -3,11 +3,12 @@
 ####This is a quick script that allow you to specify multiple devices that need to be reindex
 ##when you get a messege in the log like You have 2 Devices with bad SNMP Indexes you can try this script to 
 ##reindex the devices all you need is the device ID enter like so ./reindex 1 2 3
-###
+###Create a file with all your id's and done.
+## 
 
 
 #!/bin/bash
-for id in "$@"
+for id in $(cat /folder/file.txt)
 do
 php /var/www/html/cacti/cli/poller_reindex_hosts.php --id=$id --debug
 done
